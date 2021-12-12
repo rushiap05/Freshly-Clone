bdd65dd0025416308d0126e4283a339a26f62d0f
rushiap05 <rushiapohare@gmail.com>
2021-12-12 10:56:59 +0530

    footer javascript completed

---
+++ All javascript files/footer.js
@@ -0,0 +1,61 @@
+function footer() {
+    var foot = `<div class="border-top">
+    <div class="row container m-auto my-5">
+        <div class="col">
+            <img class="footer-main-img"
+                src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg">
+            <p class="my-4">© Freshly Inc.</p>
+        </div>
+        <div class="col">
+            <p class="lead fw-bold">Company</p>
+            <p>Careers</p>
+            <p>Contact Us.</p>
+            <p>Press</p>
+            <p>Terms</p>
+            <p>Privacy</p>
+            <p>Affiliates</p>
+            <p>Partnerships</p>
+        </div>
+        <div class="col">
+            <p class="lead fw-bold">Learn More</p>
+            <p>Plans & Menu</p>
+            <p>Why Freshly?</p>
+            <p>Gifts</p>
+            <p>FAQs</p>
+            <p>Blog</p>
+            <p>Students</p>
+        </div>
+        <div class="col">
+            <p class="lead fw-bold">Join Us</p>
+            <div class="row" style="width: 180px;">
+                <div class="col">
+                    <img class="footer-img-social"
+                        src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423bbc67c09879c5a19b_5d795c37fedbb6017d824b02_icons8-facebook-old-30.jpg">
+                </div>
+                <div class="col">
+                    <img class="footer-img-social"
+                        src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423ad92101de48f410c7_5d10dfc0ef515f74b875a42c_icons8-instagram-50.jpg">
+                </div>
+                <div class="col">
+                    <img class="footer-img-social"
+                        src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423bd050163202b341e8_5d10dfc00442bfc99d3d6828_icons8-twitter-filled-50.jpg">
+                </div>
+                <div class="col">
+                    <img class="footer-img-social"
+                        src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423a6b26e039d505425e_5d10dfbfef515faad975a42b_icons8-pinterest-filled-50.jpg">
+                </div>
+            </div>
+            <div>
+                <img class="footer-store-img"
+                    src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5d6439d4fb92e7630e46976b_Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg">
+            </div>
+            <div>
+                <img class="footer-store-img"
+                    src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5d643dd558306bfe1df5ef8b_google-play-badge.png">
+            </div>
+        </div>
+    </div>
+</div>`
+}
+
+export default footer 
