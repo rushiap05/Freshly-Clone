1c94175956e0786d0c031b472f98feb31d20e18b
rushiap05 <rushiapohare@gmail.com>
2021-12-16 16:45:52 +0530

    plan page some data added

--- All javascript files/plan.js
+++ All javascript files/plan.js
@@ -1,12 +1,48 @@
-let container = document.getElementById('plan-container');
+var container = document.getElementById('plan-container');
 
 
-async function showproduct() {
-    var res = await fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/classify?locale=en_us")
+var searchFood = async () => {
 
-    var data = await res.json()
+   try {
+    var response = await fetch("http://www.themealdb.com/api/json/v1/1/search.php?f=c");
 
-    console.log(data);
+    var data = await response.json();
+    // console.log(data);
+    return data;
+   }
+   catch(err) {
+    console.log(err);
+   }
 }
 
-showproduct()
+var appendFood = async () => {
+    var food_data = await searchFood();
+    console.log(food_data);
+
+    food_data.meals.forEach((food) => {
+        let div1 = document.createElement('div1');
+
+        if(food_data.meals === null){
+            return;
+        }
+
+        let img = document.createElement('img');
+        img.src = food.strMealThumb;
+        img.classList.add('foodImg')
+
+        let h3 = document.createElement('h3');
+        h3.innerText = "Country: " + food.strArea;
+        h3.classList.add('foodCountry')
+
+        let p = document.createElement('p');
+        p.innerText = food.strCategory;
+        p.classList.add('foodName')
+
+        div1.append(img,h3,p);
+        container.append(div1);
+    });
+}
+
+// showproduct()
+
+appendFood()
