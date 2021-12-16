ab3cd4f5fab4d86223c58bf59cf70049157ecf27
rushiap05 <rushiapohare@gmail.com>
2021-12-16 22:35:01 +0530

    some js part done

---
+++ All javascript files/buyFood.js
@@ -0,0 +1,48 @@
+var container = document.getElementById('main-container');
+
+
+var searchFood = async () => {
+
+   try {
+    var response = await fetch("http://www.themealdb.com/api/json/v1/1/search.php?f=c");
+
+    var data = await response.json();
+    // console.log(data);
+    return data;
+   }
+   catch(err) {
+    console.log(err);
+   }
+}
+
+var appendFood = async () => {
+    var food_data = await searchFood();
+    console.log(food_data);
+
+    food_data.meals.forEach((food) => {
+        let div1 = document.createElement('div1');
+
+        if(food_data.meals === null){
+            return;
+        }
+
+        let img = document.createElement('img');
+        img.src = food.strMealThumb;
+        img.classList.add('foodImg')
+
+        let h3 = document.createElement('h3');
+        h3.innerText = "Country: " + food.strArea;
+        h3.classList.add('foodCountry')
+
+        let p = document.createElement('p');
+        p.innerText = food.strCategory;
+        p.classList.add('foodName')
+
+        div1.append(img,h3,p);
+        container.append(div1);
+    });
+}
+
+// showproduct()
+
+appendFood()
