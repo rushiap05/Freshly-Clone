3999430f852494772cee035b7d66b23ceeb8cc94
rushiap05 <rushiapohare@gmail.com>
2021-12-16 23:28:43 +0530

    food js product added

--- All javascript files/buyFood.js
+++ All javascript files/buyFood.js
@@ -21,6 +21,7 @@ var appendFood = async () => {
 
     food_data.meals.forEach((food) => {
         let div1 = document.createElement('div1');
+        div1.classList.add('show-food-div1')
 
         if(food_data.meals === null){
             return;
@@ -31,14 +32,40 @@ var appendFood = async () => {
         img.classList.add('foodImg')
 
         let h3 = document.createElement('h3');
-        h3.innerText = "Country: " + food.strArea;
-        h3.classList.add('foodCountry')
+        h3.innerText = food.strMeal;
+        h3.classList.add('foodName')
 
         let p = document.createElement('p');
-        p.innerText = food.strCategory;
-        p.classList.add('foodName')
+        p.innerText = "with " + food.strTags;
+        p.classList.add('foodTags')
 
-        div1.append(img,h3,p);
+        let div3 = document.createElement('div')
+        div3.classList.add('show-food-div3');
+
+        div3.append(h3,p)
+
+        let div2 = document.createElement('div');
+        div2.classList.add('adding-button-div')
+        
+        let span1 = document.createElement("span")
+        span1.innerText = "gluten free";
+
+        let span2 = document.createElement("span")
+        span2.innerText = "490 cals";
+
+        let span3 = document.createElement("span")
+        span3.innerText = "49g carbs";
+        
+        let span4 = document.createElement("span")
+        span4.innerText = "28g protein";
+
+        let button_add = document.createElement('button');
+        button_add.innerText = "+Add"
+        button_add.classList.add('btn-primary')
+
+        div2.append(span1,span2,span3,span4,button_add)
+
+        div1.append(img,div3,div2);
         container.append(div1);
     });
 }
